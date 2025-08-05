e388ce7b7530a221fc327ae156f673209cf84141
Joseph <JPMeehan5@Gmail.com>
2025-08-04 23:46:01 -0700

    Initialized project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ds-foundryvtt-converter
-A converter from the Steel Compendium to FoundryVTT
+# Steel Compendium to FoundryVTT Converter
+
+A converter from Forge Steel and the Steel Compendium to FoundryVTT
+
+Draw Steel System: https://github.com/MetaMorphic-Digital/draw-steel
+
+Steel Compendium: https://github.com/SteelCompendium
+
+Forge Steel: https://andyaiken.github.io/forgesteel/
---
+++ scripts/forge-steel/parser.mjs
@@ -0,0 +1,16 @@
+// A script to convert abilities from the Forge Steel JSON to a Foundry-legible format
+import * as fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
+
+if (fs.existsSync("config.yaml")) {
+  const fc = await fs.promises.readFile("config.yaml", "utf-8");
+
+  const fileConfig = yaml.load(fc);
+
+  const fsfile = await fs.promises.readFile(fileConfig.forgeSteel);
+
+  const fsjson = JSON.parse(fsfile);
+
+  console.log(fsjson.name);
+}
